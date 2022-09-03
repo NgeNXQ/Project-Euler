1184aac538209f6a65ec8c1505bd7e565a6f890f
denis.babich.acc@gmail.com <denis.babich.acc@gmail.com>
2022-09-03 03:13:14 +0300

    10th done

--- Program.cs
+++ Program.cs
@@ -1,13 +1,12 @@
-﻿using System.Collections.Generic;
-using System.Runtime.Intrinsics.X86;
-
-namespace ProjectEuler
+﻿namespace ProjectEuler
 {
     internal class Program
     {
         static void Main()
         {
-            Console.WriteLine(GetPrimeNumberByIndex(10001));
+            Console.WriteLine(GetSumOfPrimes(2));
+
+
         }
 
         #region 1
@@ -159,7 +158,7 @@ namespace ProjectEuler
             Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is .
             Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
         */
-        
+
         static int CalculateProblemSix()
         {
             return GetSquareOfSum() - GetSumOfSquares();
@@ -192,7 +191,7 @@ namespace ProjectEuler
             By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
             What is the 10 001st prime number?
         */
-        
+
         static int GetPrimeNumberByIndex(int number)
         {
             int currentNumber = 1;
@@ -230,5 +229,135 @@ namespace ProjectEuler
         }
 
         #endregion
+
+        #region 8
+
+        //The four adjacent digits in the 1000-digit number that have the greatest product are 9 × 9 × 8 × 9 = 5832.
+
+        //73167176531330624919225119674426574742355349194934
+        //96983520312774506326239578318016984801869478851843
+        //85861560789112949495459501737958331952853208805511
+        //12540698747158523863050715693290963295227443043557
+        //66896648950445244523161731856403098711121722383113
+        //62229893423380308135336276614282806444486645238749
+        //30358907296290491560440772390713810515859307960866
+        //70172427121883998797908792274921901699720888093776
+        //65727333001053367881220235421809751254540594752243
+        //52584907711670556013604839586446706324415722155397
+        //53697817977846174064955149290862569321978468622482
+        //83972241375657056057490261407972968652414535100474
+        //82166370484403199890008895243450658541227588666881
+        //16427171479924442928230863465674813919123162824586
+        //17866458359124566529476545682848912883142607690042
+        //24219022671055626321111109370544217506941658960408
+        //07198403850962455444362981230987879927244284909188
+        //84580156166097919133875499200524063689912560717606
+        //05886116467109405077541002256983155200055935729725
+        //71636269561882670428252483600823257530420752963450
+
+        //Find the thirteen adjacent digits in the 1000-digit number that have the greatest product.What is the value of this product?
+
+        //    private static int GetLargestNumber(string text, int nubmerOfDigits)
+        //    {
+        //        int countOfNumbers = text.Length / nubmerOfDigits;
+        //        int[] numbers = new int[countOfNumbers];
+
+        //        for (int i = 0; i < countOfNumbers; ++i)
+        //            numbers[i] = 1;
+
+        //        int index = 0;
+        //        int count = 0;
+
+        //        for (int i = 0, length = text.Length; i < length; ++i)
+        //        {
+        //            while (count++ < nubmerOfDigits)
+        //                numbers[i] *= 
+        //        }
+
+        //        int SplitNumberByDigitsAndMultiply(int number)
+        //        {
+        //            int result = 1;
+        //            int digit;
+
+        //            while (number > 1)
+        //            {
+        //                digit = number % 10;
+        //                number = (number - digit) / 10;
+
+        //                result *= digit;
+        //            }
+
+        //            return result;
+        //        }
+
+        //        return -1;
+        //    }
+
+        //}
+
+        #endregion
+
+        #region 9
+
+        /*
+            A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+
+                                        a^2 + b^2 = c^2
+
+            For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+            There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+            Find the product abc.
+        */
+
+        //private static int GetProductOfPythagoreanTripletForSelectedNumber(int number)
+        //{
+        //    for (int a = 1; a < 1000; ++a)
+        //    {
+
+        //    }
+        //}
+
+        #endregion
+
+        #region 10
+
+        /*
+            The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+            Find the sum of all the primes below two million.
+        */
+
+        private static ulong GetSumOfPrimes(int number)
+        {
+            ulong result = 0;
+
+            for (int i = 1; i < number; ++i)
+            {
+                if (IsPrime(i))
+                    result += (ulong)i;
+            }
+
+            bool IsPrime(int number)
+            {
+                if (number == 1)
+                    return false;
+                else if (number == 2)
+                    return true;
+                else
+                {
+                    for (int i = 2, length = number; i < length; ++i)
+                    {
+                        if (number % i == 0)
+                            return false;
+                    }
+                }
+
+                return true;
+            }
+
+            return result;
+        }
+
+        #endregion
+
     }
 }
